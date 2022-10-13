70f7016dee7283037bcbc09811ece2dd9d27a3f4
Guillem Gracia <ggraciadev@gmail.com>
2022-10-13 21:28:28 +0200

    playing with colors

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,32 @@
 #include "bn_core.h"
 #include "bn_keypad.h"
 #include <bn_sprite_ptr.h>
+#include <bn_regular_bg_ptr.h>
+#include <bn_sprite_tiles_ptr.h>
 
-#include "bn_sprite_items_character.h"
-#include "bn_sprite_items_cobblestone.h"
+#include "bn_sprite_items_main_character.h"
+#include "bn_sprite_items_bolardo.h"
+#include "bn_sprite_items_farola.h"
+#include "bn_regular_bg_items_back.h"
 
 
 int main()
 {
     bn::core::init();
     int posX = 0;
-    int posY = 0;
-    bn::sprite_ptr sprite = bn::sprite_items::character.create_sprite(posX, posY);
+    int posY = 50;
+    bn::sprite_ptr sprite = bn::sprite_items::main_character.create_sprite(posX, posY);
+    sprite.set_z_order(0);
+    
+    bn::sprite_ptr farola1 = bn::sprite_items::farola.create_sprite(-50, 0);
+    bn::sprite_ptr farola2 = bn::sprite_items::farola.create_sprite(-50, 64);
 
-    bn::sprite_ptr ground = bn::sprite_items::cobblestone.create_sprite(1 << -1, 15);
+    bn::sprite_ptr bolardo = bn::sprite_items::bolardo.create_sprite(20, 60);
+
+    bn::regular_bg_ptr back = bn::regular_bg_items::back.create_bg(10, 50);
+
+    farola2.set_tiles(bn::sprite_items::farola.tiles_item().create_tiles(1));
+          
 
     while(true)
     {
@@ -32,9 +45,6 @@ int main()
         else if(bn::keypad::down_held())
         {
             ++posY;
-            if(posY > 15 - 32 - 8) {
-                posY = 15 - 32 - 8;
-            }
         }
         sprite.set_x(posX);
         sprite.set_y(posY);
